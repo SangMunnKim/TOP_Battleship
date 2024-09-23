2d922dc1981cb25af72b401cbbfea5c3d2e3a3b1
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-09-23 13:26:34 +0200

    add: gameboard and ship to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./styles.css";
-console.log("Hello World");
+import { gameboard } from "./components/gameboard";
+import { Ship } from "./components/battleship";
+
+const board = gameboard();
+
+const ship_1 = new Ship(3);
+const ship_2 = new Ship(4);
+
+board.placeShip(ship_1, 0, 0, 'horizontal');
+board.placeShip(ship_2, 2, 2, 'vertical');
+
+board.render();
