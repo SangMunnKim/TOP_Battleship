ac63dd7684182b1833c5221a1fa0bc4bcc160262
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-09-23 13:26:10 +0200

    feat: Add gameboard and test files

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,19 +1,50 @@
 function gameboard(){
-    const grid = Array(10).fill(null).map(() => Array(10).fill(null));
+    const grid = Array(10).fill(null).map(() => Array(10).fill(null).map(() => ({ship: null, hit: false})));
     
     return {
         getGrid() {
             return grid;
         },
 
-        placeShip() {
-            // code here
+        placeShip(ship, x, y, direction) {
+            for (let i = 0; i < ship.getLength(); i++) {
+                if (direction === 'horizontal') {
+                    grid[x][y + i].ship = ship;
+                } else if (direction === 'vertical') {
+                    grid[x + i][y].ship = ship;
+                }
+            }
         },
 
-        receiveAttack() {
-            // code here
+        attack(x, y) {
+            if (grid[x][y].hit) {
+                console.log('already hit');
+            } else if (grid[x][y].ship) {
+                grid[x][y].ship.hit();
+                grid[x][y].hit = true;
+                console.log('hit');
+            } else {
+                grid[x][y].hit = true;
+                console.log('miss');
+            }
+        },
+
+        allShipsSunk() {
+            return grid.flat().filter(cell => cell.ship).every(cell => cell.ship.isSunk());
+        },
+
+        render() {
+            grid.forEach(row => {
+              console.log(row.map(cell => {
+                if (cell.ship) {
+                  return cell.hit ? 'X' : 'S';  // 'S' for unhit ship, 'X' for hit ship
+                } else {
+                  return cell.hit ? 'O' : '.';  // 'O' for miss, '.' for untouched cell
+                }
+              }).join(' '));
+            });
         }
     }
 }
 
-module.exports = gameboard;
+export { gameboard };
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,4 +1,5 @@
-const gameboard = require('../src/components/gameboard');
+import { gameboard } from '../src/components/gameboard';
+import { Ship } from '../src/components/battleship';
 
 test('test if gameboard has correct size', () => {
     const board = gameboard();
@@ -8,3 +9,60 @@ test('test if gameboard has correct size', () => {
         expect(board.getGrid()[row].length).toBe(10);
     }
 });
+
+test('test if ship is placed correctly', () => {
+    const board = gameboard();
+    const ship = new Ship(3);
+    const ship2 = new Ship(5);
+    board.placeShip(ship, 0, 0, 'horizontal');
+    board.placeShip(ship2, 1, 0, 'vertical');
+    expect(board.getGrid()[0][0].ship).toBe(ship);
+    expect(board.getGrid()[0][1].ship).toBe(ship);
+    expect(board.getGrid()[0][2].ship).toBe(ship);
+    expect(board.getGrid()[0][3].ship).toBe(null);
+
+    expect(board.getGrid()[1][0].ship).toBe(ship2);
+    expect(board.getGrid()[2][0].ship).toBe(ship2);
+    expect(board.getGrid()[3][0].ship).toBe(ship2);
+    expect(board.getGrid()[4][0].ship).toBe(ship2);
+    expect(board.getGrid()[5][0].ship).toBe(ship2);
+    expect(board.getGrid()[6][0].ship).toBe(null);
+});
+
+test('test if ship is hit', () => {
+    const board = gameboard();
+    const ship = new Ship(3);
+    board.placeShip(ship, 0, 0, 'horizontal');
+    board.attack(0, 0);
+    board.attack(0, 1);
+    expect(board.getGrid()[0][0].hit).toBe(true);
+    expect(board.getGrid()[0][1].hit).toBe(true);
+    expect(ship.isHit()).toBe(2);
+});
+
+test('test if ship is sunk', () => {
+    const board = gameboard();
+    const ship = new Ship(3);
+    board.placeShip(ship, 0, 0, 'horizontal');
+    board.attack(0, 0);
+    board.attack(0, 1);
+    board.attack(0, 2);
+    expect(ship.isSunk()).toBe(true);
+});
+
+test('test if all ships are sunk', () => {
+    const board = gameboard();
+    const ship = new Ship(3);
+    const ship2 = new Ship(3);
+    board.placeShip(ship, 0, 0, 'horizontal');
+    board.placeShip(ship2, 1, 0, 'horizontal');
+    board.attack(0, 0);
+    board.attack(0, 1);
+    board.attack(0, 2);
+    board.attack(1, 0);
+    board.attack(1, 1);
+    board.attack(1, 2);
+    expect(board.allShipsSunk()).toBe(true);
+});
+
+
