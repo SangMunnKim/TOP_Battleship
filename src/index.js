c12ec19c1f2d5262568a6c5c7079bdd6fb1b4218
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-09-24 17:25:25 +0200

    add: gameboard UI and event listener on start button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import "./styles.css";
 import { gameboard } from "./factories/gameboard";
 import { Ship } from "./factories/battleship";
+import { startGame } from "./components/eventlisteners";
 
-const board = gameboard();
+init();
 
-const ship_1 = new Ship(3);
-const ship_2 = new Ship(4);
-
-board.placeShip(ship_1, 0, 0, 'horizontal');
-board.placeShip(ship_2, 2, 2, 'vertical');
-
-board.render();
+function init() {
+    startGame;
+};
